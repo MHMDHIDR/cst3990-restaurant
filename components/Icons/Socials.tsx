966ed79eb150bc8d7e18fa6db189bb45b9800bfe
Mohammed Haydar <mr.hamood277@gmail.com>
2023-12-27 02:18:05 +0000

    Style: Twitter Icon modified to X

--- components/Icons/Socials.tsx
+++ components/Icons/Socials.tsx
@@ -4,13 +4,17 @@ const ICON_SIZE_CLASS =
 //Twitter
 export const Twitter = ({ fill = 'white' }) => (
   <svg
-    className={`${ICON_SIZE_CLASS} opacity-60 hover:opacity-100 transition-opacity mx-auto`}
-    xmlns='http://www.w3.org/2000/svg'
-    viewBox='0 0 512 512'
+    stroke='currentColor'
     fill={fill}
+    stroke-width='0'
+    viewBox='0 0 24 24'
+    className={`${ICON_SIZE_CLASS} inline-block opacity-60 hover:opacity-100 transition-opacity mx-auto`}
+    height='1em'
+    width='1em'
+    xmlns='http://www.w3.org/2000/svg'
   >
     <title>Twitter Page</title>
-    <path d='M496 109.5a201.8 201.8 0 01-56.55 15.3 97.51 97.51 0 0043.33-53.6 197.74 197.74 0 01-62.56 23.5A99.14 99.14 0 00348.31 64c-54.42 0-98.46 43.4-98.46 96.9a93.21 93.21 0 002.54 22.1 280.7 280.7 0 01-203-101.3A95.69 95.69 0 0036 130.4c0 33.6 17.53 63.3 44 80.7A97.5 97.5 0 0135.22 199v1.2c0 47 34 86.1 79 95a100.76 100.76 0 01-25.94 3.4 94.38 94.38 0 01-18.51-1.8c12.51 38.5 48.92 66.5 92.05 67.3A199.59 199.59 0 0139.5 405.6a203 203 0 01-23.5-1.4A278.68 278.68 0 00166.74 448c181.36 0 280.44-147.7 280.44-275.8 0-4.2-.11-8.4-.31-12.5A198.48 198.48 0 00496 109.5z' />
+    <path d='M8 2H1L9.26086 13.0145L1.44995 21.9999H4.09998L10.4883 14.651L16 22H23L14.3917 10.5223L21.8001 2H19.1501L13.1643 8.88578L8 2ZM17 20L5 4H7L19 20H17Z'></path>
   </svg>
 )
 
